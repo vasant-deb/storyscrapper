440964f0a17006692f31cd21a4b475ba83710204
vasant-deb <iamvasantsharma@gmail.com>
2022-09-06 11:15:06 -0600

    m1

--- app.py
+++ app.py
@@ -4,25 +4,15 @@ app = Flask(__name__)
 
 @app.route('/getmsg/', methods=['GET'])
 def respond():
-    # Retrieve the name from the url parameter /getmsg/?name=
-    name = request.args.get("name", None)
+    import requests
 
-    # For debugging
-    print(f"Received: {name}")
+    URL = "https://realpython.github.io/fake-jobs/"
+    page = requests.get(URL)
 
-    response = {}
-
-    # Check if the user sent a name at all
-    if not name:
-        response["ERROR"] = "No name found. Please send a name."
-    # Check if the user entered a number
-    elif str(name).isdigit():
-        response["ERROR"] = "The name can't be numeric. Please send a string."
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome API!"
+    print(page.text)
 
     # Return the response in json format
-    return jsonify(response)
+    return jsonify(page.text)
 
 
 @app.route('/post/', methods=['POST'])
